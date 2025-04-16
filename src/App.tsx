801a9abdbffddb92a32f18e3072a42d8ca03966a
ArturGraefenstein <13917629+ArturGraefenstein@users.noreply.github.com>
2025-04-16 21:45:20 +0200

    fix full size page

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import Header from "./components/Header";
 
 function App() {
   return (
-    <main className="min-h-screen flex flex-col items-center font-sans bg-gradient-to-br from-blue-50 via-white to-neutral-100 text-neutral-900 px-4 sm:px-8">
+    <main className="min-h-screen flex flex-col items-center font-sans bg-[#292F36] text-neutral-900 w-full">
       <Header />
     </main>
   );
