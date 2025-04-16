f1c5f85668a8fe3d1318474aee108e49c9962a79
ArturGraefenstein <13917629+ArturGraefenstein@users.noreply.github.com>
2025-04-16 23:12:35 +0200

    Entferne Home-Link aus Header (Navbar entfernt), da bereits auf Startseite

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,4 @@
 import Logo from "./Logo";
-import Navbar from "./Navbar";
 import SocialMediaBar from "./SocialMediaBar";
 import Divider from "./Divider";
 
@@ -7,11 +6,8 @@ const Header = () => (
   <header className="w-full bg-[#292F36] overflow-x-hidden">
     <div className="max-w-screen-2xl mx-auto w-full flex flex-row justify-between items-center py-16 px-4 md:px-[128px] gap-8 md:gap-[120px]">
       <Logo />
-      <div className="flex flex-row items-center gap-8 md:gap-16 flex-shrink-0">
-        <Navbar />
-        <div className="flex flex-row items-center gap-4 md:gap-8 flex-shrink-0">
-          <SocialMediaBar />
-        </div>
+      <div className="flex flex-row items-center gap-4 md:gap-8 flex-shrink-0">
+        <SocialMediaBar />
       </div>
     </div>
     <Divider />
