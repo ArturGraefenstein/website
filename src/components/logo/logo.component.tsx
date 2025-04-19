558e74009c79578820085ade84d0d2bccf540cf4
ArturGraefenstein <13917629+ArturGraefenstein@users.noreply.github.com>
2025-04-19 18:08:15 +0200

    compackt logo

--- src/components/logo/logo.component.tsx
+++ src/components/logo/logo.component.tsx
@@ -8,7 +8,7 @@ import {
 export const Logo = () => {
   return (
     <div className={logoContainerClass}>
-      <span className={`${logoTextClass} ${bracketClass}`}>&lt;AG /&gt;</span>
+      <span className={`${logoTextClass} ${bracketClass}`}>&lt;AG/&gt;</span>
       <span className={`${logoTextClass} ${nameClass}`}>Artur Gräfenstein</span>
     </div>
   );
